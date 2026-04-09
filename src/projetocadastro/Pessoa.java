54ff39b9d9a7bcca205f1ccf9db0a093d12240a8
Laura Gouveia Nelis <1094407@senacminas.edu.br>
2026-04-09 20:09:32 -0300

    first commit

---
+++ src/projetocadastro/Pessoa.java
@@ -0,0 +1,42 @@
+
+
+public class Pessoa {
+    
+    private String nome;
+    private int idade;
+    private int cpf;
+    
+    public Pessoa(String nome, int idade, int cpf){
+        this.nome = nome;
+        this.cpf = cpf ;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+
+    
+
+}
---
+++ src/projetocadastro/ProjetoCadastro.java
@@ -0,0 +1,12 @@
+
+package projetocadastro;
+
+
+public class ProjetoCadastro {
+    
+    public static void main(String[] args) {
+        
+        
+    }
+
+}
