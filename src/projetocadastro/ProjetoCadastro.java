1cb35a95c48c38f5555f5b3e28e3eae012c52108
Jordan Armond de Lima <1080275@senacminas.edu.br>
2026-04-10 20:31:29 -0300

    oi

--- src/projetocadastro/ProjetoCadastro.java
+++ src/projetocadastro/ProjetoCadastro.java
@@ -3,7 +3,13 @@ package projetocadastro;
 
 
 public class ProjetoCadastro {
+    //atributos
+    private String nome;
+    private int idade;
+    private String cpf;
     
+    
+            
     public static void main(String[] args) {
         
         
